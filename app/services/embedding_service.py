668a0add79698f1fabd228b7dfb8983f103c2964
Di-Heqiu <18303611177@163.com>
2026-04-16 09:57:34 +0800

    0416

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -86,7 +86,7 @@ class EmbeddingService:
     
     def _load_model(self):
         """在线程池中执行的模型加载"""
-        return FlagModel.from_finetuned(
+        return FlagModel(
             self.model_name,
             query_instruction_for_retrieval=self.query_instruction,
             use_fp16=True  # 使用半精度加速
